037b2daf1379d85d56419b33561fd0b645c7e416
Peter Adelmann <peter.adelmann@trilobit.de>
2023-05-03 11:49:29 +0200

    fix bundle class

--- src/TrilobitCollaborationtoolnotificationBundle.php
+++ src/TrilobitCollaborationtoolnotificationBundle.php
@@ -12,12 +12,6 @@ namespace Trilobit\CollaborationtoolnotificationBundle;
 
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Trilobit\WorldofimagesBundle\DependencyInjection\WorldofimagesExtension;
-
 class TrilobitCollaborationtoolnotificationBundle extends Bundle
 {
-    public function getContainerExtension(): ?ExtensionInterface
-    {
-        return new WorldofimagesExtension();
-    }
 }
